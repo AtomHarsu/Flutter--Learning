de66f48e734fbcffb3282fc536ee67b33aaadf8a
GorHarshu <120025277+AtomHarsu@users.noreply.github.com>
2023-04-19 15:23:33 +0530

    main screen

--- learning/lib/createblog.dart
+++ learning/lib/createblog.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:learning/const.dart';
 
+import 'mybog(homescreen).dart';
+
 class CreateBlog extends StatefulWidget {
   const CreateBlog({super.key});
 
@@ -27,33 +29,117 @@ class _CreateBlogState extends State<CreateBlog> {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            Container(
-              padding: EdgeInsets.all(8.0),
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height * 0.2,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15.0),
-                  color: colors.bgcolor),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Take Picture",
-                    style: TextStyle(
-                        letterSpacing: 3,
-                        fontWeight: FontWeight.bold,
-                        color: colors.fontcolor),
-                  ),
-                  Icon(
-                    Icons.picture_in_picture,
-                    color: colors.fontcolor,
-                  ),
-                ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.2,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    color: colors.bgcolor),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Take Picture",
+                      style: TextStyle(
+                          letterSpacing: 3,
+                          fontWeight: FontWeight.bold,
+                          color: colors.fontcolor),
+                    ),
+                    Icon(
+                      Icons.picture_in_picture,
+                      color: colors.fontcolor,
+                    ),
+                  ],
+                ),
               ),
-            )
+            ),
+            getEmailTextFiled(),
+            getEmailTextFiled2(),
+            appLoginButton(context)
           ],
         ),
       ),
     );
   }
+}
+
+getEmailTextFiled() {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: TextField(
+      controller: TextEditingController(),
+      style: TextStyle(color: colors.bgcolor),
+      decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.deepPurple),
+              borderRadius: BorderRadius.circular(15)),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.deepPurple),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          labelStyle: TextStyle(color: colors.bgcolor),
+          fillColor: colors.fontcolor,
+          prefixIcon: Icon(
+            Icons.email,
+            color: colors.bgcolor,
+          ),
+          labelText: "Blog Title",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(15))),
+    ),
+  );
+}
+
+getEmailTextFiled2() {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: TextField(
+      maxLines: 5,
+      controller: TextEditingController(),
+      style: TextStyle(color: colors.bgcolor),
+      decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.deepPurple),
+              borderRadius: BorderRadius.circular(15)),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.deepPurple),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          labelStyle: TextStyle(color: colors.bgcolor),
+          fillColor: colors.fontcolor,
+          prefixIcon: Icon(
+            Icons.email,
+            color: colors.bgcolor,
+          ),
+          labelText: "Blog Description",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(15))),
+    ),
+  );
+}
+
+appLoginButton(BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: InkWell(
+      onTap: () {
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (_) => HomeScreen()));
+      },
+      child: Container(
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height * 0.05,
+        decoration: BoxDecoration(
+          color: colors.bgcolor,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Center(
+            child: Text(
+          'Create',
+          style:
+              TextStyle(color: colors.fontcolor, fontWeight: FontWeight.bold),
+        )),
+      ),
+    ),
+  );
 }
--- learning/lib/mybog(homescreen).dart
+++ learning/lib/mybog(homescreen).dart
@@ -59,6 +59,89 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         centerTitle: true,
       ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height * 0.2,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.0),
+                        color: colors.bgcolor),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                "Title:",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: colors.fontcolor),
+                              ),
+                              Text(
+                                "abcd",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: colors.fontcolor),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "Description:",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: colors.fontcolor),
+                              ),
+                              Expanded(
+                                child: Text(
+                                  "Taken from the Latin words dolorem ipsum, which translates to pain itself, Lorem Ipsum text saw a revival in the mid-20th century as placeholder text in printing and design.",
+                                  overflow: TextOverflow.ellipsis,
+                                  softWrap: true,
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      color: colors.fontcolor),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10)),
+                              margin: EdgeInsets.all(10.0),
+                              width: double.infinity,
+                              height: MediaQuery.of(context).size.height * 0.1,
+                              child: Image.asset(
+                                "asstes/20943790.jpg",
+                                fit: BoxFit.cover,
+                              ))
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
