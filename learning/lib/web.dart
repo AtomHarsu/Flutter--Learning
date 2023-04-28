63a63b171cac70fe32c06b1ca15d284bfa378b04
GorHarshu <120025277+AtomHarsu@users.noreply.github.com>
2023-04-28 16:14:57 +0530

    testing changes

--- learning/lib/web.dart
+++ learning/lib/web.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 
 class WebLogin extends StatefulWidget {
   const WebLogin({super.key});
@@ -9,110 +10,184 @@ class WebLogin extends StatefulWidget {
 }
 
 class _WebLoginState extends State<WebLogin> {
+  bool isPressed = false;
+  void buttonTap() {
+    setState(() {
+      isPressed = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: SafeArea(
-        child: Row(
-          children: [
-            Expanded(
-              flex: 1,
-              child: Container(),
-            ),
-            Expanded(
-              flex: 1,
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Icon(
-                      Icons.android,
+      body: Center(
+        child: GestureDetector(
+          onTap: buttonTap,
+          child: isPressed
+              ? Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey[300],
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey[500]!,
+                          offset: Offset(4, 4),
+                          blurRadius: 15.0,
+                          spreadRadius: 1.0),
+                      BoxShadow(
+                          color: Colors.white,
+                          offset: Offset(-4, -4),
+                          blurRadius: 15.0,
+                          spreadRadius: 1.0)
+                    ],
+                  ),
+                  child: Center(
+                    child: Icon(
+                      Icons.apple,
                       size: 100,
                     ),
-                    Text(
-                      'Hello Again',
-                      style: GoogleFonts.poppins(
-                        fontSize: 52,
-                      ),
+                  ))
+              : Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.all(Radius.circular(16)),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey[500]!,
+                          offset: Offset(4, 4),
+                          blurRadius: 15.0,
+                          spreadRadius: 1.0),
+                      BoxShadow(
+                          color: Colors.white,
+                          offset: Offset(-4, -4),
+                          blurRadius: 15.0,
+                          spreadRadius: 1.0)
+                    ],
+                  ),
+                  child: Center(
+                    child: Icon(
+                      Icons.apple,
+                      size: 100,
                     ),
-                    const SizedBox(
-                      height: 10,
+                  ),
+                ),
+        ),
+      ),
+    );
+  }
+
+  SafeArea newMethod(BuildContext context) {
+    return SafeArea(
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 250,
+                    height: 250,
+                    child: Lottie.network(
+                        'https://assets9.lottiefiles.com/private_files/lf30_m6j5igxb.json'),
+                  ),
+                  Text(
+                    'Hello Again',
+                    style: GoogleFonts.bebasNeue(
+                      fontSize: 20,
                     ),
-                    Text(
-                      'Welcome back ,You have been missed',
-                      style: GoogleFonts.bebasNeue(
-                        fontSize: 18,
-                      ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Welcome back ,You have been missed',
+                    style: GoogleFonts.bebasNeue(
+                      fontSize: 18,
                     ),
-                    const SizedBox(
-                      height: 18,
+                  ),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 25.0,
                     ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 25.0,
-                      ),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(
-                              color: Colors.white,
-                            ),
-                            borderRadius: BorderRadius.circular(
-                              12,
-                            ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Colors.white,
                           ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(
-                              color: Colors.purple,
-                            ),
-                            borderRadius: BorderRadius.circular(
-                              12,
-                            ),
+                          borderRadius: BorderRadius.circular(
+                            12,
                           ),
-                          labelText: "Email*",
-                          labelStyle: TextStyle(color: Colors.purple),
-                          fillColor: Colors.grey[200],
-                          filled: true,
                         ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 25.0,
-                      ),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(
-                              color: Colors.white,
-                            ),
-                            borderRadius: BorderRadius.circular(
-                              12,
-                            ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Colors.purple,
                           ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(
-                              color: Colors.purple,
-                            ),
-                            borderRadius: BorderRadius.circular(
-                              12,
-                            ),
+                          borderRadius: BorderRadius.circular(
+                            12,
                           ),
-                          labelText: "Password*",
-                          labelStyle: TextStyle(color: Colors.purple),
-                          fillColor: Colors.grey[200],
-                          filled: true,
                         ),
+                        labelText: "Email*",
+                        labelStyle: TextStyle(color: Colors.purple),
+                        fillColor: Colors.grey[200],
+                        filled: true,
                       ),
                     ),
-                    SizedBox(
-                      height: 10,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 25.0,
                     ),
-                    Padding(
+                    child: TextFormField(
+                      obscureText: true,
+                      obscuringCharacter: "*",
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Colors.white,
+                          ),
+                          borderRadius: BorderRadius.circular(
+                            12,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Colors.purple,
+                          ),
+                          borderRadius: BorderRadius.circular(
+                            12,
+                          ),
+                        ),
+                        labelText: "Password*",
+                        labelStyle: TextStyle(color: Colors.purple),
+                        fillColor: Colors.grey[200],
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Padding(
                         padding: const EdgeInsets.symmetric(
                           horizontal: 25.0,
                         ),
@@ -132,17 +207,17 @@ class _WebLoginState extends State<WebLogin> {
                                   fontSize: 16),
                             ),
                           ),
-                        ))
-                  ],
-                ),
+                        )),
+                  )
+                ],
               ),
             ),
-            Expanded(
-              flex: 1,
-              child: Container(),
-            ),
-          ],
-        ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(),
+          ),
+        ],
       ),
     );
   }
