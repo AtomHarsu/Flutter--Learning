da6040d4da70903e13759654d4aca47d25192943
GorHarshu <120025277+AtomHarsu@users.noreply.github.com>
2023-05-03 16:48:56 +0530

    make an alert Dialog

--- learning/lib/mobilelogin.dart
+++ learning/lib/mobilelogin.dart
@@ -14,59 +14,164 @@ class _LoginMobileState extends State<LoginMobile> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      elevation: 0,
-                      backgroundColor: Colors.transparent,
-                      behavior: SnackBarBehavior.floating,
-                      content: CustomSnackBar(
-                          icon: Icons.check_circle_outline,
-                          colors: Colors.green,
-                          text: 'Welcome to The Flutter Community',
-                          title: 'Hello!Flutter Dev'),
-                    ),
-                  );
-                },
-                child: const Text('Snackbar Success')),
-            ElevatedButton(
-                onPressed: () {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      elevation: 0,
-                      backgroundColor: Colors.transparent,
-                      behavior: SnackBarBehavior.floating,
-                      content: CustomSnackBar(
-                          icon: Icons.close,
-                          colors: Colors.red,
-                          text: 'Welcome to The Flutter Community',
-                          title: 'Hello!Flutter Dev'),
-                    ),
-                  );
-                },
-                child: const Text('Snackbar Error')),
-            ElevatedButton(
-                onPressed: () {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      elevation: 0,
-                      backgroundColor: Colors.transparent,
-                      behavior: SnackBarBehavior.floating,
-                      content: CustomSnackBar(
-                          icon: Icons.report,
-                          colors: Colors.orange,
-                          text: 'Welcome to The Flutter Community',
-                          title: 'Hello!Flutter Dev'),
-                    ),
-                  );
-                },
-                child: const Text('Snackbar Alert')),
-          ],
-        ),
+          child: ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      contentPadding: const EdgeInsets.all(0.0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0)),
+                      backgroundColor: Colors.green,
+                      title: Column(
+                        children: [
+                          Container(
+                            width: 75,
+                            height: 75,
+                            decoration: const BoxDecoration(
+                                shape: BoxShape.circle, color: Colors.white),
+                            child: const Icon(
+                              Icons.done,
+                              color: Colors.green,
+                              size: 50,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                        ],
+                      ),
+                      content: Stack(
+                        children: [
+                          Container(
+                            decoration: const BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(16),
+                                  bottomRight: Radius.circular(16),
+                                )),
+                            height: MediaQuery.of(context).size.height * 0.18,
+                            width: double.infinity,
+                            child: Column(
+                              children: [
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                const Text(
+                                  'Success',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                const Text(
+                                  'Transaction completed succesfully!',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(
+                                  height: 30,
+                                ),
+                                InkWell(
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: Container(
+                                    width: MediaQuery.of(context).size.width *
+                                        0.22,
+                                    height: MediaQuery.of(context).size.height *
+                                        0.045,
+                                    decoration: const BoxDecoration(
+                                        color: Colors.cyan,
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(16))),
+                                    child: const Center(
+                                        child: Text(
+                                      'Okay',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    )),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
+              child: const Text('Show Dialog'))),
+    );
+  }
+}
+
+class NewWidgetSetSnackBar extends StatelessWidget {
+  const NewWidgetSetSnackBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    elevation: 0,
+                    backgroundColor: Colors.transparent,
+                    behavior: SnackBarBehavior.floating,
+                    content: CustomSnackBar(
+                        icon: Icons.check_circle_outline,
+                        colors: Colors.green,
+                        text: 'Welcome to The Flutter Community',
+                        title: 'Hello!Flutter Dev'),
+                  ),
+                );
+              },
+              child: const Text('Snackbar Success')),
+          ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    elevation: 0,
+                    backgroundColor: Colors.transparent,
+                    behavior: SnackBarBehavior.floating,
+                    content: CustomSnackBar(
+                        icon: Icons.close,
+                        colors: Colors.red,
+                        text: 'Welcome to The Flutter Community',
+                        title: 'Hello!Flutter Dev'),
+                  ),
+                );
+              },
+              child: const Text('Snackbar Error')),
+          ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    elevation: 0,
+                    backgroundColor: Colors.transparent,
+                    behavior: SnackBarBehavior.floating,
+                    content: CustomSnackBar(
+                        icon: Icons.report,
+                        colors: Colors.orange,
+                        text: 'Welcome to The Flutter Community',
+                        title: 'Hello!Flutter Dev'),
+                  ),
+                );
+              },
+              child: const Text('Snackbar Alert')),
+        ],
       ),
     );
   }
